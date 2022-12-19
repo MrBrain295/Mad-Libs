d884df63f5fd349fb0492e9e50f2afb2049813a6
Gabe <66077254+MrBrain295@users.noreply.github.com>
2022-12-18 19:50:20 -0600

    Update mad-libs.py

--- mad-libs.py
+++ mad-libs.py
@@ -1,5 +1,6 @@
-place = input("Enter a place: ")
-animal = input("Enter a animal:")
-job = input("Enter the name of a job:")
-badsmell = input("Enter a bad smell:")
-print(f"We went to {place} to see a {animal} but a {job}er said go away. I said why? They said you smell like {badsmell}")
+def get_output_string():
+  place, animal, job, badsmell = input("Enter a place, animal, job, and bad smell: ").split()
+  return f"We went to {place} to see a {animal} but a {job}er said go away. I said why? They said you smell like {badsmell}"
+
+output_string = get_output_string()
+print(output_string)
